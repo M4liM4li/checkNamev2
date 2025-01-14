79390bf859a425b12ee09b5a1508916382d1f97c
Mali Mali <siwkrnesgyix1@gmail.com>
2025-01-14 21:48:02 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const authRouter = require("./routes/auth");
-const attendanceRouter = require("./routes/Attendance");
+const { readdirSync } = require("fs");
 
-//middleware
+// middleware
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
@@ -15,19 +14,28 @@ app.use(
     credentials: true,
   })
 );
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Server is running' });
 });
 
-app.use("/api", authRouter);
-app.use("/api", attendanceRouter);
+// Dynamically load all route files in the routes directory
+readdirSync("./routes").forEach((item) => {
+  if (item.endsWith(".js")) { // Check if the file is a .js file
+    const route = require(`./routes/${item}`);
+    app.use("/api", route);
+  }
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({
     message: "Internal Server Error",
-    details: err.message, // สามารถส่งข้อความเพิ่มเติมจากข้อผิดพลาด
+    details: err.message, // ส่งข้อความเพิ่มเติมจากข้อผิดพลาด
   });
 });
-app.listen(5000, () => console.log("server running port 5000x"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
 
 module.exports = app;
