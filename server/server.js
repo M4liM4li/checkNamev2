db77d95f2978c5f53d9db0dae4d4f92dd9b2ebe8
Mali Mali <siwkrnesgyix1@gmail.com>
2025-01-14 20:41:57 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const { readdirSync } = require("fs");
 const cors = require("cors");
-// const authRouter = require('./routes/auth')
-// const categoryRouter = require('./routes/category')
+const authRouter = require("./routes/auth");
+const attendanceRouter = require("./routes/attendanceRouter");
 //middleware
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
-
-// app.use('/api', authRouter)
-// app.use('/api', categoryRouter)
-readdirSync("./routes").map((item) =>
-  app.use("/api", require("./routes/" + item))
+app.use(
+  cors({
+    origin: "*", // หรือ URL ของแอป React ของคุณ
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
 );
+
+app.use("/api", authRouter);
+app.use("/api", attendanceRouter);
 
 app.listen(5000, () => console.log("server running port 5000"));
